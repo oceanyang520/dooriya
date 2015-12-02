3c706b7796bcdb84c731a0a9df2221f1811afcf9
小杨 <yangsea@aliyun.com>
2015-12-02 21:21:16 +0800

    commit blog

---
+++ src/main/java/com/ocean/frame/main/action/ManagerAction.java
@@ -0,0 +1,23 @@
+package com.ocean.frame.main.action;
+
+public class ManagerAction {
+
+    public String login(){
+        
+        return "manager/";
+    }
+    
+     
+    //权限控制，后台使用bootstrap
+    public String getMenuByRoleId(){
+        return "";
+    }
+    
+    public String getUserList(){
+        
+        return "";
+    }
+    
+    
+    
+}
