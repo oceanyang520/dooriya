eb3c5ad731e93e3bb054bfe718189c968dd841ff
小杨 <yangsea@aliyun.com>
2016-04-13 18:03:04 +0800

    aaa

--- src/test/java/simpletest/Test.java
+++ src/test/java/simpletest/Test.java
@@ -1,8 +1,36 @@
 package simpletest;
 
+import java.util.Collection;
+import java.util.Map;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.core.DistributedObject;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+
 public class Test {
 
     public static void main(String[] args) {
+        HazelcastInstance ins = Hazelcast.newHazelcastInstance();
+        Map<String, Customer> mapCustomers =  ins.getMap("cusotmers");
+        mapCustomers.put("1", new Customer("Joe", 2));
+        mapCustomers.put("2", new Customer("Ali", 3));
+        mapCustomers.put("3", new Customer("Avi", 4));
+         
+//        Collection<Customer> colCustomers = mapCustomers.values();
+//        for (Customer customer : colCustomers) {
+//            // process customer
+//            System.out.println(customer.getName());
+//        }
+        Collection<DistributedObject> dos = ins.getDistributedObjects();
+        for (DistributedObject distributedObject : dos) {
+            if(dos instanceof IMap){
+                System.out.println(distributedObject.getName());
+            }
+        }
+        
+//        Config conf = new Config();
         
     }
 }
